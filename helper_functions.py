e92613dc5d93a0d732ca72631cd458195439daf2
Niranjan Shah <niranjanshah474@gmail.com>
2023-05-31 18:57:26 +1000

    Model trained and prediction done

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
+import numpy as np
 
 def pre_process_tweet(tweet):
     #converting to lowercase
@@ -31,8 +32,35 @@ def pre_process_tweet(tweet):
     stop_words = set(stopwords.words('english'))
     cleaned_tweet_tokens = [word for word in tweet_tokens if word not in stop_words ]
 
-    # stemming
-    # stemmer = PorterStemmer()
-    # stemmed_tweets = [stemmer.stem(word) for word in cleaned_tweet_tokens]
+
 
     return cleaned_tweet_tokens
+# convert each word in sentence to embedding using word2vec model
+def word_to_vec(sentence, word2vec_model):
+    return [word2vec_model.wv[word] if word in word2vec_model.wv else np.zeros(word2vec_model.vector_size) for word in sentence]
+def get_pre_processed_input(tweet,word2vec_model,maxlen=37,embedding_dim=100):
+    """
+    function to pre_process the input tweet that includes converting to embedding and getting data in required format by nn model
+    """
+    # maxlen is 37
+    # embedding_dim is 100 which is the vector size of word2vec model
+    # Tokenize and pad the tweet
+    pre_processed_new_tweet = pre_process_tweet(tweet)
+    #getting embedding of each word of tweet
+    vectorized_new_tweet =  word_to_vec(pre_processed_new_tweet,word2vec_model)
+    # padding to get uniform size inout
+    vectorized_new_tweet_padded = pad_sequence(vectorized_new_tweet, word2vec_model, maxlen)
+    # reshaping the tweet to match input shape of model
+    vectorized_new_tweet_padded = vectorized_new_tweet_padded.reshape(1, maxlen, embedding_dim)
+
+
+    return vectorized_new_tweet_padded
+
+# used to have uniform input by padding the input which are smaller
+def pad_sequence(seq, word2vec_model, maxlen=37):
+    """
+    Function to convert input into uniform size by padding the input which are smaller than maxlen
+    """
+    maxlen = 37
+    return np.array(seq + [np.zeros(word2vec_model.vector_size)] * (maxlen - len(seq)))
+
